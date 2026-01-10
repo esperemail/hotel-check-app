4fc1f9214aa0e9e666fb689f3d5491c4678fa33f
esperemail <esperemail@gmail.com>
2026-01-10 14:55:03 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('hotel-check-v1').then(cache => cache.addAll([
+      './index.html',
+      './manifest.json'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
